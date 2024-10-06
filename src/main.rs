5cf49cf0791a0bb5bcc0011e8c361c23e1cd7e9a
HankB-o-t <t37.slime.1.1@gmail.com>
2024-10-06 20:28:41 -0300

    upd

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use std::io::{stdin, stdout, Write};use std::process::Command;use std::env;use std::path::Path;
-fn read(input: &mut String) {let _ = stdout().flush();stdin().read_line(input).unwrap();}
-const TEXT: [&str; 10] = ["  ██████  ██░ ██ ▓█████  ██▓     ██▓          ██▀███    ██████ ","▒██    ▒ ▓██░ ██▒▓█   ▀ ▓██▒    ▓██▒         ▓██ ▒ ██▒▒██    ▒ ","░ ▓██▄   ▒██▀▀██░▒███   ▒██░    ▒██░         ▓██ ░▄█ ▒░ ▓██▄   ","  ▒   ██▒░▓█ ░██ ▒▓█  ▄ ▒██░    ▒██░         ▒██▀▀█▄    ▒   ██▒","▒██████▒▒░▓█▒░██▓░▒████▒░██████▒░██████▒ ██▓ ░██▓ ▒██▒▒██████▒▒","▒ ▒▓▒ ▒ ░ ▒ ░░▒░▒░░ ▒░ ░░ ▒░▓  ░░ ▒░▓  ░ ▒▓▒ ░ ▒▓ ░▒▓░▒ ▒▓▒ ▒ ░","░ ░▒  ░ ░ ▒ ░▒░ ░ ░ ░  ░░ ░ ▒  ░░ ░ ▒  ░ ░▒    ░▒ ░ ▒░░ ░▒  ░ ░","░  ░  ░   ░  ░░ ░   ░     ░ ░     ░ ░    ░     ░░   ░ ░  ░  ░  ","      ░   ░  ░  ░   ░  ░    ░  ░    ░  ░  ░     ░           ░  ","                                          ░                    "];
-fn main() {for row in 0..10 {println!("{}", TEXT[row]);}loop {print!("                               $ ");let mut i=String::new();read(&mut i);let mut p=i.trim().split_whitespace();let cm=p.next().unwrap();let a=p; if cm=="exit"{break;}match cm{"cd"=>{let nd=a.peekable().peek().map_or("/",|x|*x);let r=Path::new(nd);if let Err(e)=env::set_current_dir(&r){eprintln!("{}", e);}},cm=>{let mut c=Command::new(cm).args(a).spawn().unwrap();c.wait();}}}}
+use std::io::{stdin,stdout,Write};use std::process::Command;/////////////
+use std::env;use std::path::Path; fn read(input:&mut String)/////////////
+{let _=stdout().flush();stdin().read_line(input).unwrap();}fn //shell.rs/
+main(){loop{print!("$ ");let mut i=String::new();read(&mut i);///////////
+let mut p=i.trim().split_whitespace();let cm=p.next().unwrap()//2024/////
+;let a=p;if cm=="exit"{break;}match cm{"cd"=>{let nd=a.//////////////////
+peekable().peek().map_or("/",|x|*x);let r=Path::new(nd);////HankB-o-t////
+if let Err(e)=env::set_current_dir(&r){eprintln!("{}",e);////////////////
+}},cm=>{let mut c=Command::new(cm).args(a).spawn().unwrap();c.wait();}}}}
